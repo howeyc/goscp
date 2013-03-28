abe2149b62d8f63b783db77a3d7022d7aa09c40a
Chris Howey <chris@howey.me>
2013-03-28 13:32:43 -0500

    Allow progress for Reader / Writer, closing only if implements Closer interface.

--- progressIO.go
+++ progressIO.go
@@ -9,7 +9,7 @@ import (
 )
 
 type writeProgress struct {
-	embed    io.WriteCloser
+	embed    io.Writer
 	progress *pb.ProgressBar
 	start    time.Time
 }
@@ -23,11 +23,14 @@ func (wp *writeProgress) Write(p []byte) (n int, err error) {
 func (wp *writeProgress) Close() error {
 	dur := time.Since(wp.start)
 	wp.progress.FinishPrint(fmt.Sprintf("File transfer completed with an average rate of %f kB/s", (float64(wp.progress.Total)/1024.0)/dur.Seconds()))
-	return wp.embed.Close()
+	if cls, ok := wp.embed.(io.Closer); ok {
+		return cls.Close()
+	}
+	return nil
 }
 
 type readProgress struct {
-	embed    io.ReadCloser
+	embed    io.Reader
 	progress *pb.ProgressBar
 	start    time.Time
 }
@@ -41,5 +44,8 @@ func (rp *readProgress) Read(p []byte) (n int, err error) {
 func (rp *readProgress) Close() error {
 	dur := time.Since(rp.start)
 	rp.progress.FinishPrint(fmt.Sprintf("File transfer completed with an average rate of %f kB/s", (float64(rp.progress.Total)/1024.0)/dur.Seconds()))
-	return rp.embed.Close()
+	if cls, ok := rp.embed.(io.Closer); ok {
+		return cls.Close()
+	}
+	return nil
 }
